b67c7dad9aa59a4d351e38e1070235a5c4b63f60
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-12 17:28:21 +0900

    added props to Pet component

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,22 +1,22 @@
-const Pet = () => {
+// props aka properties, allows our React components to be more flexible!
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h2", {}, "Ash"),
-    React.createElement("h3", {}, "Chai"),
-    React.createElement("h3", {}, "Honey"),
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
   ])
 }
-
 
 // App is a React component. It's like a rubber stamp (something that can be re-used). It is critical that we capitalise the name of our components.
 const App = () => {
   return React.createElement(
     "div",
-    { }, // If we want to pass our div any attributes i.e. a class or an id, this is where would do it.
+    {}, // If we want to pass our div any attributes i.e. a class or an id, this is where would do it.
     [
       React.createElement("h1", { }, "Adopt Me!"),
-      React.createElement(Pet),
-      React.createElement(Pet),
-      React.createElement(Pet),
+      React.createElement(Pet, { name: "Honey", animal: "Dog", breed: "Poodle" }),
+      React.createElement(Pet, { name: "IB", animal: "Bird", breed: "Budgie" }),
+      React.createElement(Pet, { name: "Sora-Momo", animal: "Cat", breed: "Blue Persian" }),
     ] // These array brackets are optional.
     );
 };
