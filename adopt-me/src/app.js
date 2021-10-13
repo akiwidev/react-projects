736547a63a2fca7b452c1dc08ac4a8993a411e8f
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-13 14:17:20 +0900

    initialised npm, and installed prettier

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -4,8 +4,8 @@ const Pet = (props) => {
     React.createElement("h2", {}, props.name),
     React.createElement("h3", {}, props.animal),
     React.createElement("h3", {}, props.breed),
-  ])
-}
+  ]);
+};
 
 // App is a React component. It's like a rubber stamp (something that can be re-used). It is critical that we capitalise the name of our components.
 
@@ -15,12 +15,20 @@ const App = () => {
     "div",
     {}, // If we want to pass our div any attributes i.e. a class or an id, this is where would do it.
     [
-      React.createElement("h1", { }, "Adopt Me!"),
-      React.createElement(Pet, { name: "Honey", animal: "Dog", breed: "Poodle" }),
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        name: "Honey",
+        animal: "Dog",
+        breed: "Poodle",
+      }),
       React.createElement(Pet, { name: "IB", animal: "Bird", breed: "Budgie" }),
-      React.createElement(Pet, { name: "Sora-Momo", animal: "Cat", breed: "Blue Persian" }),
+      React.createElement(Pet, {
+        name: "Sora-Momo",
+        animal: "Cat",
+        breed: "Blue Persian",
+      }),
     ] // These array brackets are optional.
-    );
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
