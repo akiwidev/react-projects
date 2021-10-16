1ef44598731459ab6f4a314dbe12f2bb6bfc92a8
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-16 12:08:12 +0900

    added extra options to .eslintrc.json file to accomodate react and jsx

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
@@ -7,25 +6,39 @@ import Pet from "./Pet";
 // App is a React component. It's like a rubber stamp (something that can be re-used). It is critical that we capitalise the name of our components.
 
 // Data flows down! So I can pass properties from the App component to the Pet component, but not the other way around. ONE WAY DATA FLOW Parent > Child.
-const App = () => {
-  return React.createElement(
-    "div",
-    {}, // If we want to pass our div any attributes i.e. a class or an id, this is where would do it.
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Honey",
-        animal: "Dog",
-        breed: "Poodle",
-      }),
-      React.createElement(Pet, { name: "IB", animal: "Bird", breed: "Budgie" }),
-      React.createElement(Pet, {
-        name: "Sora-Momo",
-        animal: "Cat",
-        breed: "Blue Persian",
-      }),
-    ] // These array brackets are optional.
-  );
-};
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+// const App = () => {
+//   return React.createElement(
+//     "div",
+//     {}, // If we want to pass our div any attributes i.e. a class or an id, this is where would do it.
+//     [
+//       React.createElement("h1", {}, "Adopt Me!"),
+//       React.createElement(Pet, {
+//         name: "Honey",
+//         animal: "Dog",
+//         breed: "Poodle",
+//       }),
+//       React.createElement(Pet, { name: "IB", animal: "Bird", breed: "Budgie" }),
+//       React.createElement(Pet, {
+//         name: "Sora-Momo",
+//         animal: "Cat",
+//         breed: "Blue Persian",
+//       }),
+//     ] // These array brackets are optional.
+//   );
+// };
+
+// re-writing the above code in JSX format (so much more readable!!)
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      {/*Below is how we pass props to our component in JSX*/}
+      <Pet name="Honey" animal="Dog" breed="Poodle" />
+      <Pet name="IB" animal="Bird" breed="Budgie" />
+      <Pet name="Sora-Momo" animal="Cat" breed="Blue Persian" />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
