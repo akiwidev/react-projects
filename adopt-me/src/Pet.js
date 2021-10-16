b02a6384b920d68ecbba6d24a375a57c8a34450c
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-16 11:36:41 +0900

    rewrote Pet.js in JSX format

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 // props aka properties, allows our React components to be more flexible!
+
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h3", {}, props.animal),
+//     React.createElement("h3", {}, props.breed),
+//   ]);
+// };
+
+// The code below is JSX (JavaScript XML). Instead of putting JavaScript into HTML, JSX allows us to put HTML into JavaScript. It is a syntax extension of React, and allows us to directly write HTML in React. When run through babel it will output the same code as above ☝️
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  );
+}
 
 export default Pet;
