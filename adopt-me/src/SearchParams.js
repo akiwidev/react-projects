d5d06c3ced0179317e35d58e3a4d0859c0f0105e
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-16 13:28:41 +0900

    added updateanimal hook to searchParams component

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,10 +1,16 @@
-// hooks always begin with 'use..' and track the state of something. Never put hooks inside of for loops or if statements!
+// Hooks always begin with 'use..' and track the state of something. Whenever some kind of input changes, React kicks off a re-render cycle, and it's gonna call the hooks.
+// Never put hooks inside of for loops or if statements!
 
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "reptile", "fish", "kangaroo"]
+
 const SearchParams = () => {
+  {/* 95% of the time your hooks are going to be written here, at the top of your component. */ }
+
   {/* With destructuriing */ }
-  const [location, setLocation] = useState("Te Awamutu")
+  const [location, setLocation] = useState("Te Awamutu");
+  const [animal, updateAnimal] = useState("");
 
   {/* Without destructuring */ }
   // const locationTuple = useState("Te Awamutu");
@@ -20,6 +26,21 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
             value={location}
             placeholder="Location" />
+        </label>
+        <label htmlFor="animal">Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => updateAnimal(e.target.value)}
+            onBlur={e => updateAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
