8816c25f930724383cfe9ba3f37b1356fecc66ab
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-17 14:34:38 +0900

    refactored Pet component

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,24 +1,33 @@
-import React from "react";
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
+  if(images.length) {
+    hero = images[0];
+  }
 
-// props aka properties, allows our React components to be more flexible!
+  return (
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} — ${breed} — ${location}`}</h2>
+      </div>
+    </a>
+  );
+};
 
-// const Pet = (props) => {
+{/* The code above is JSX (JavaScript XML). Instead of putting JavaScript into HTML, JSX allows us to put HTML into JavaScript. It is a syntax extension of React, and allows us to directly write HTML in React. When run through babel it will output the same code as below👇 */}
+
+{/* const Pet = (props) => {
 //   return React.createElement("div", {}, [
 //     React.createElement("h2", {}, props.name),
 //     React.createElement("h3", {}, props.animal),
 //     React.createElement("h3", {}, props.breed),
 //   ]);
-// };
+// }; */}
 
-// The code below is JSX (JavaScript XML). Instead of putting JavaScript into HTML, JSX allows us to put HTML into JavaScript. It is a syntax extension of React, and allows us to directly write HTML in React. When run through babel it will output the same code as above ☝️
-const Pet = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.animal}</h3>
-      <h3>{props.breed}</h3>
-    </div>
-  );
-}
+{/* props aka properties, allows our React components to be more flexible! */}
+
 
 export default Pet;
