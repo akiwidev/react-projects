06db5f1c889c47f258d58e68edbef37cba47f054
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-19 14:11:26 +0900

    added errorBoundary class componenent and added it to Details component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -1,11 +1,12 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
 
 class Details extends Component {
   state = { loading: true };
 
-  async componentDidMount () {
+  async componentDidMount() {
     const response = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
     );
@@ -19,14 +20,22 @@ class Details extends Component {
       )
     );
   }
-  render () {
-    if(this.state.loading){
+  render() {
+    if (this.state.loading) {
       return <h2>loading...</h2>
     }
-    const { animal, breed, city, state, description, name, images } = this.state;
-   return (
+    const {
+      animal,
+      breed,
+      city,
+      state,
+      description,
+      name,
+      images } = this.state;
+
+    return (
       <div className="details">
-        <Carousel images={images}/>
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{animal} - {breed} - {city}, {state}</h2>
@@ -39,4 +48,11 @@ class Details extends Component {
 }
 
 // This is called a higher-order component. By wrapping Details inside withRouter, the Router is going to pass on all the information (aka props) into Details.
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  )
+}
