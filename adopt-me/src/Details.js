79d26de7f3aded54a9db37c3b26f0aae9bcf82ea
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-10-19 15:28:41 +0900

    fixed typo in modal component

--- adopt-me/src/Details.js
+++ adopt-me/src/Details.js
@@ -3,9 +3,10 @@ import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
-  state = { loading: true };
+  state = { loading: true, showModal: false };
 
   async componentDidMount() {
     const response = await fetch(
@@ -21,6 +22,10 @@ class Details extends Component {
       )
     );
   }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal })
+  adopt = () => (window.location = "http://bit.ly/pet-adopt");
+
   render() {
     if (this.state.loading) {
       return <h2>loading...</h2>
@@ -32,7 +37,8 @@ class Details extends Component {
       state,
       description,
       name,
-      images } = this.state;
+      images,
+      showModal } = this.state;
 
     return (
       <div className="details">
@@ -42,10 +48,23 @@ class Details extends Component {
           <h2>{animal} - {breed} - {city}, {state}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button onClick={this.toggleModal} style={{ backgroundColor: theme }}>Adopt {name}</button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {
+            showModal ? (
+              <Modal>
+                <div>
+                  <h1>Would you like to adopt {name}?</h1>
+                  <div className="buttons">
+                    <button onClick={this.adopt}>Hell yeah!</button>
+                    <button onClick={this.toggleModal}>Hell no!</button>
+                  </div>
+                </div>
+              </Modal>
+            ) : null
+          }
         </div>
       </div>
     )
--- adopt-me/src/Modal.js
+++ adopt-me/src/Modal.js
@@ -1,19 +1,20 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const modalRoot = document.getElementById('model');
+const modalRoot = document.getElementById("modal");
 
 const Modal = ({ children }) => {
   const elRef = useRef(null);
   if (!elRef.current) {
-    elRef.current = document.createElement('div');
+    elRef.current = document.createElement("div");
   }
+
   useEffect(() => {
     modalRoot.appendChild(elRef.current);
     return () => modalRoot.removeChild(elRef.current);
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
-}
+};
 
 export default Modal;
