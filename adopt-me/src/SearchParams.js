c478c12c5d4f1c0513e96f15d360734a8bcf69bd
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-03 12:00:32 +0900

    added tailwind css to search params, as well as disabled breed option unless animal is selected

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -4,14 +4,18 @@
 import { useState, useEffect, useContext } from "react";
 import ThemeContext from "./ThemeContext";
 import useBreedList from "./useBreedList";
-import Results from "./Results"
+import Results from "./Results";
 
-const ANIMALS = ["bird", "cat", "dog", "reptile", "rabbit"]
+const ANIMALS = ["bird", "cat", "dog", "reptile", "rabbit"];
 
 const SearchParams = () => {
-  {/* 95% of the time your hooks are going to be written here, at the top of your component. */ }
+  {
+    /* 95% of the time your hooks are going to be written here, at the top of your component. */
+  }
 
-  {/* Hooks with destructuring */ }
+  {
+    /* Hooks with destructuring */
+  }
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
@@ -19,7 +23,9 @@ const SearchParams = () => {
   const [breeds] = useBreedList(animal);
   const [theme, setTheme] = useContext(ThemeContext);
 
-  {/* Hooks without destructuring */ }
+  {
+    /* Hooks without destructuring */
+  }
   // const locationTuple = useState("Te Awamutu");
   // const location = locationTuple[0];
   // const setLocation = locationTuple[1];
@@ -40,60 +46,70 @@ const SearchParams = () => {
   async function requestPets() {
     const response = await fetch(
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-    )
+    );
     const json = await response.json();
 
     setPets(json.pets);
   }
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
-        onSubmit={e => {
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center divide-y divide-gray-900"
+        onSubmit={(e) => {
           e.preventDefault();
           requestPets();
         }}
       >
-        <label htmlFor="location">Location
+        <label className="search-label" htmlFor="location">
+          Location
           <input
+            className="search-content"
             id="location"
             onChange={(e) => setLocation(e.target.value)}
             value={location}
-            placeholder="Location" />
+            placeholder="Location"
+          />
         </label>
-        <label htmlFor="animal">Animal
+        <label className="search-label" htmlFor="animal">
+          Animal
           <select
+            className="search-content"
             id="animal"
             value={animal}
-            onChange={e => setAnimal(e.target.value)}
-            onBlur={e => setAnimal(e.target.value)}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
           >
             <option />
-            {ANIMALS.map(animal => (
+            {ANIMALS.map((animal) => (
               <option value={animal} key={animal}>
                 {animal}
               </option>
             ))}
           </select>
         </label>
-        <label htmlFor="breed">Breed
+        <label className="search-label" htmlFor="breed">
+          Breed
           <select
+            className="search-content disabled:opacity-50"
+            disabled={!breeds.length}
             id="breed"
             value={breed}
-            onChange={e => setBreed(e.target.value)}
-            onBlur={e => setBreed(e.target.value)}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
           >
             <option />
-            {breeds.map(breed => (
+            {breeds.map((breed) => (
               <option value={breed} key={breed}>
                 {breed}
               </option>
             ))}
           </select>
         </label>
-        <label htmlFor="theme">
+        <label className="search-label" htmlFor="theme">
           Theme
           <select
+            className="search-content"
             value={theme}
             onChange={(e) => setTheme(e.target.value)}
             onBlur={(e) => setTheme(e.target.value)}
@@ -104,11 +120,16 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
+        <button
+          className="rounded px-6 py-2 text-white hover:opacity-50 border-none"
+          style={{ backgroundColor: theme }}
+        >
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
-  )
-}
+  );
+};
 
 export default SearchParams;
