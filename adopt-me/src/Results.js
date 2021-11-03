af1a2aa69840e8091074f768ef305e1578fa4af2
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-03 12:13:49 +0900

    added grid and breakpoints to results component

--- adopt-me/src/Results.js
+++ adopt-me/src/Results.js
@@ -2,24 +2,24 @@ import Pet from "./Pet";
 
 const Results = ({ pets }) => {
   return (
-    <div className="search">
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {!pets.length ? (
         <h2>No Pets Found</h2>
       ) : (
-        pets.map(pet => (
+        pets.map((pet) => (
           <Pet
-          animal={pet.animal}
-          key={pet.id}
-          name={pet.name}
-          breed={pet.breed}
-          images={pet.images}
-          location={`${pet.city}, ${pet.state}`}
-          id={pet.id}
+            animal={pet.animal}
+            key={pet.id}
+            name={pet.name}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+            id={pet.id}
           />
         ))
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Results
+export default Results;
