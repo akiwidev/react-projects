1bb6b651ed1e837c249be225b61b52c56912e5e9
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-03 12:22:18 +0900

    added tailwind css styling to pet component

--- adopt-me/src/Pet.js
+++ adopt-me/src/Pet.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
 const Pet = ({ name, animal, breed, images, location, id }) => {
-  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
-  if(images.length) {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
     hero = images[0];
   }
 
   return (
-    <Link to={`/details/${id}`} className="pet">
-      <div className="image-container">
+    <Link to={`/details/${id}`} className="relative block">
+      <div>
         <img src={hero} alt={name} />
       </div>
-      <div className="info">
+      <div className="absolute bottom-0 left-0 bg-gradient-to-tr from-white to-transparent pr-2 pt-2">
         <h1>{name}</h1>
         <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
@@ -19,17 +19,22 @@ const Pet = ({ name, animal, breed, images, location, id }) => {
   );
 };
 
-{/* The code above is JSX (JavaScript XML). Instead of putting JavaScript into HTML, JSX allows us to put HTML into JavaScript. It is a syntax extension of React, and allows us to directly write HTML in React. When run through babel it will output the same code as below👇 */}
+{
+  /* The code above is JSX (JavaScript XML). Instead of putting JavaScript into HTML, JSX allows us to put HTML into JavaScript. It is a syntax extension of React, and allows us to directly write HTML in React. When run through babel it will output the same code as below👇 */
+}
 
-{/* const Pet = (props) => {
+{
+  /* const Pet = (props) => {
 //   return React.createElement("div", {}, [
 //     React.createElement("h2", {}, props.name),
 //     React.createElement("h3", {}, props.animal),
 //     React.createElement("h3", {}, props.breed),
 //   ]);
-// }; */}
+// }; */
+}
 
-{/* props aka properties, allows our React components to be more flexible! */}
-
+{
+  /* props aka properties, allows our React components to be more flexible! */
+}
 
 export default Pet;
