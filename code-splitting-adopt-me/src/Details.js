633289a7e194124f79e0d8b31c10513267df8ee6
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-05 12:18:43 +0900

    added lazy function to Details component for Modal

--- code-splitting-adopt-me/src/Details.js
+++ code-splitting-adopt-me/src/Details.js
@@ -1,9 +1,10 @@
-import { Component } from "react";
+import { Component, lazy } from "react";
 import { withRouter } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
-import Modal from "./Modal";
+
+const Modal = lazy(() => import("./Modal"));
 
 class Details extends Component {
   state = { loading: true, showModal: false };
@@ -23,51 +24,49 @@ class Details extends Component {
     );
   }
 
-  toggleModal = () => this.setState({ showModal: !this.state.showModal })
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
   adopt = () => (window.location = "http://bit.ly/pet-adopt");
 
   render() {
     if (this.state.loading) {
-      return <h2>loading...</h2>
+      return <h2>loading...</h2>;
     }
-    const {
-      animal,
-      breed,
-      city,
-      state,
-      description,
-      name,
-      images,
-      showModal } = this.state;
+    const { animal, breed, city, state, description, name, images, showModal } =
+      this.state;
 
     return (
       <div className="details">
         <Carousel images={images} />
         <div>
           <h1>{name}</h1>
-          <h2>{animal} - {breed} - {city}, {state}</h2>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button onClick={this.toggleModal} style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
-          {
-            showModal ? (
-              <Modal>
-                <div>
-                  <h1>Would you like to adopt {name}?</h1>
-                  <div className="buttons">
-                    <button onClick={this.adopt}>Hell yeah!</button>
-                    <button onClick={this.toggleModal}>Hell no!</button>
-                  </div>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Hell yeah!</button>
+                  <button onClick={this.toggleModal}>Hell no!</button>
                 </div>
-              </Modal>
-            ) : null
-          }
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
-    )
+    );
   }
 }
 
@@ -78,5 +77,5 @@ export default function DetailsWithErrorBoundary() {
     <ErrorBoundary>
       <DetailsWithRouter />
     </ErrorBoundary>
-  )
+  );
 }
