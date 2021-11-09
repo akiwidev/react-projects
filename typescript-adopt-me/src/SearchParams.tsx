6a592f50508325df2f126341bdde39a1be33e90f
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-09 12:36:47 +0900

    migrated Results from js to typescript

--- typescript-adopt-me/src/SearchParams.tsx
+++ typescript-adopt-me/src/SearchParams.tsx
@@ -23,7 +23,7 @@ const SearchParams: FunctionComponent<RouteComponentProps> = () => {
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
-    const json = await res.json();
+    const json = (await res.json()) as PetAPIResponse;
 
     setPets(json.pets);
   }
