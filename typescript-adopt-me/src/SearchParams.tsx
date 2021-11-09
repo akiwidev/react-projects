9a72f793313ccb6494fda3bffe2c829d951d1fc6
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-09 12:28:47 +0900

    migrated SearchParams from js to typescript

--- typescript-adopt-me/src/SearchParams.tsx
+++ typescript-adopt-me/src/SearchParams.tsx
@@ -1,20 +1,22 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, FunctionComponent } from "react";
+import { RouteComponentProps } from "react-router";
 import ThemeContext from "./ThemeContext";
 import useBreedList from "./useBreedList";
 import Results from "./Results";
+import { PetAPIResponse, Animal, Pet } from "./APIResponseTypes";
 
-const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const ANIMALS: Animal[] = ["bird", "cat", "dog", "rabbit", "reptile"];
 
-const SearchParams = () => {
-  const [animal, updateAnimal] = useState("");
+const SearchParams: FunctionComponent<RouteComponentProps> = () => {
+  const [animal, updateAnimal] = useState("" as Animal);
   const [location, updateLocation] = useState("");
   const [breed, updateBreed] = useState("");
-  const [pets, setPets] = useState([]);
+  const [pets, setPets] = useState([] as Pet[]);
   const [breeds] = useBreedList(animal);
   const [theme, setTheme] = useContext(ThemeContext);
 
   useEffect(() => {
-    requestPets();
+    void requestPets();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   async function requestPets() {
@@ -31,7 +33,7 @@ const SearchParams = () => {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          requestPets();
+          void requestPets();
         }}
       >
         <label htmlFor="location">
@@ -48,8 +50,8 @@ const SearchParams = () => {
           <select
             id="animal"
             value={animal}
-            onChange={(e) => updateAnimal(e.target.value)}
-            onBlur={(e) => updateAnimal(e.target.value)}
+            onChange={(e) => updateAnimal(e.target.value as Animal)}
+            onBlur={(e) => updateAnimal(e.target.value as Animal)}
           >
             <option />
             {ANIMALS.map((animal) => (
