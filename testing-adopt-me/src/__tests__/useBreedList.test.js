7d9e6f7bcd3d3d8bf36244b1d6d12a78e8c5103a
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-12 13:30:44 +0900

    added test for custom hook useBreedList

---
+++ testing-adopt-me/src/__tests__/useBreedList.test.js
@@ -0,0 +1,23 @@
+import { expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
+import useBreedList from "../useBreedList";
+
+function getBreedList(animal) {
+  let list;
+
+  function TestComponent() {
+    list = useBreedList(animal);
+    return null;
+  }
+
+  render(<TestComponent />);
+
+  return list;
+}
+
+test("gives an empty array with no animal", async () => {
+  const [breedList, status] = getBreedList();
+
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("unloaded");
+});
