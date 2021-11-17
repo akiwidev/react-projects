18d6587143e84f69113ff5630148915e00a84f24
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-17 11:30:24 +0900

    added react skeleton to components

---
+++ budget-calculator/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      Hello from Expense Item
+    </div>
+  )
+}
+
+export default ExpenseItem
---
+++ budget-calculator/src/components/ExpenseList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseList = () => {
+  return (
+    <div>
+      Hello from expense list
+    </div>
+  )
+}
+
+export default ExpenseList
