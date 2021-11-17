8f554841d7e7c406a00d34f7faa22e1e8c9fd1bc
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-17 11:35:55 +0900

    added necessary imports to files

--- budget-calculator/src/App.js
+++ budget-calculator/src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
+import Alert from './components/Alert';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
 
 function App() {
   return (
-    <div>
-      Hello Cheri
-    </div>
+    <>
+      <Alert />
+      <ExpenseForm />
+      <ExpenseList />
+    </>
   );
 }
 
--- budget-calculator/src/components/ExpenseList.js
+++ budget-calculator/src/components/ExpenseList.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Item from './ExpenseItem';
 
 const ExpenseList = () => {
   return (
     <div>
       Hello from expense list
+      <Item/>
     </div>
   )
 }
