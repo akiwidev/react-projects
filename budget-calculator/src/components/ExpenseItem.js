74c8ffa7faa0aa61ba4287b90045a96fab18e14b
cheri kennedy <cheri.k3nn3dy@gmail.com>
2021-11-19 12:35:16 +0900

    set up basic skeleton for expense item component, and added edit and delete buttons

--- budget-calculator/src/components/ExpenseItem.js
+++ budget-calculator/src/components/ExpenseItem.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {MdEdit, MdDelete} from 'react-icons/md';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({expense}) => {
+  const {id, charge, amount } = expense;
   return (
-    <div>
-      Hello from Expense Item
-    </div>
+    <li className="item">
+      <div className="info">
+        <span className="expense">{charge}</span>
+        <span className="amount">${amount}</span>
+      </div>
+      <div>
+        <button className="edit-btn" aria-label="edit button"><MdEdit /></button>
+        <button className="delete-btn" aria-label="delete button"><MdDelete /></button>
+      </div>
+    </li>
   )
 }
 
